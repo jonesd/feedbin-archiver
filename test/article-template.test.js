9bb570f907358072635d6907a944ab19fecc76d0
David Jones <david@dgjones.info>
2017-04-28 12:10:57 -0400

    Extract article template

--- test/article-template.test.js
+++ test/article-template.test.js
@@ -1,3 +1,59 @@
-/**
- * Created by jonesd on 2017-04-28.
- */
+const should = require('should');
+
+const articleTemplate = require('../lib/article-template');
+articleTemplate.init();
+
+describe('article-template', function() {
+  var article;
+  beforeEach(function() {
+    article = {
+      id: 123456789,
+      title: 'test article',
+      author: 'test author',
+      content: '<p>some content</p><img src="picture.jpg">',
+      url: 'http://test.com/article',
+      "published": "2014-07-29T10:46:31.000000Z",
+      "created_at": "2014-07-29T11:06:04.740551Z"
+    }
+  });
+
+  describe('render', function() {
+    it('contains title template', function() {
+      var html = articleTemplate.render(article);
+      html.should.containEql('<title>test article</title>');
+      html.should.containEql('<h1>test article</h1>');
+    });
+    it('contains title template', function() {
+      var html = articleTemplate.render(article);
+      html.should.containEql('<title>test article</title>');
+      html.should.containEql('<h1>test article</h1>');
+    });
+    it('contains author', function() {
+      var html = articleTemplate.render(article);
+      html.should.containEql('<h2>test author</h2>');
+    });
+    it('contains content', function() {
+      var html = articleTemplate.render(article);
+      html.should.containEql('<p>some content</p><img src="picture.jpg">');
+    });
+    it('can override article content', function() {
+      article.content = 'original content';
+      var html = articleTemplate.render(article, '<p>Overide Content</p>');
+      html.should.containEql('<p>Overide Content</p>');
+      html.should.not.containEql(article.content);
+    });
+    it('contains original url', function() {
+      var html = articleTemplate.render(article);
+      html.should.containEql('<p>Original URL: http://test.com/article</p>');
+    });
+    it('contains published timestamp', function() {
+      var html = articleTemplate.render(article);
+      html.should.containEql('<p>Date: 2014-07-29T10:46:31.000000Z</p>');
+    });
+    it('faills back to created timestamp when published missing', function() {
+      article.published = null;
+      var html = articleTemplate.render(article);
+      html.should.containEql('<p>Date: 2014-07-29T11:06:04.740551Z</p>');
+    });
+  });
+});
